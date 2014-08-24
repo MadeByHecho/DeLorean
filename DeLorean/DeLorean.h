3825ae9b188d431d57d1fb3926e3c87f422c4ab4
Scott Petit <petit.scott@gmail.com>
2014-08-24 08:53:58 +0100

    Initial Commit

---
+++ DeLorean/DeLorean.h
@@ -0,0 +1,19 @@
+//
+//  DeLorean.h
+//  DeLorean
+//
+//  Created by Scott Petit on 8/24/14.
+//  Copyright (c) 2014 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeLorean.
+FOUNDATION_EXPORT double DeLoreanVersionNumber;
+
+//! Project version string for DeLorean.
+FOUNDATION_EXPORT const unsigned char DeLoreanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeLorean/PublicHeader.h>
+
+
